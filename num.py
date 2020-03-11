d92f485585640c3b99f0b2f402f67bbd368848c0
Adam Kmet <adamkmet97@gmail.com>
2020-03-11 12:03:15 +0000

    .

--- num.py
+++ num.py
@@ -1,7 +1,19 @@
+import json , requests
 example = "B00"
 
 inp = "B00095430"
+number = example + inp[3:]
+day = "Wed"
+response = requests.get('https://adamkmet1997.github.io/json_hosting/data.json')
+data = json.loads(response.text)
+print(number)
 
-if example in inp:
-    cleaned = inp[3:]
-    print(example + cleaned)
+for v in data:
+    if number in v['sNumber']:
+        answer = v["subject"],v["time"],v["room"]
+print(answer)
+
+# if example in inp:
+#     cleaned = inp[3:]
+#     print(example + cleaned)
+
